b7d97eaaa3630bde4c51a4d15c459b5a5fbbb198
dpzhoufeng <dpzhoufeng@163.com>
2019-07-26 11:40:58 +0800

    引入数据库连接池依赖

--- src/main/java/com/xreal/controller/UserController.java
+++ src/main/java/com/xreal/controller/UserController.java
@@ -10,16 +10,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/User")
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     UserService userService;
 
-    @RequestMapping("ID/{id}")
+    @RequestMapping("id/{id}")
     public @ResponseBody String getUser(@PathVariable String id) {
         User user = userService.getById(Long.valueOf(id));
-
-        return "Hello" + user.getUsername();
+        if(user!=null){
+        return "Hello" + user.getUsername();}
+        else {
+            return "找不到这个id!";
+        }
     }
 }
