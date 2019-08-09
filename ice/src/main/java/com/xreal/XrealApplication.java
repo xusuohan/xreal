54f30d62e18896fe1e08e53f1c5e865e3e28dff5
dpzhoufeng <dpzhoufeng@163.com>
2019-08-09 13:38:15 +0800

    添加定时任务执行功能

--- ice/src/main/java/com/xreal/XrealApplication.java
+++ ice/src/main/java/com/xreal/XrealApplication.java
@@ -2,13 +2,13 @@ package com.xreal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-
+@EnableScheduling
 public class XrealApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(XrealApplication.class, args);
 	}
-
 }
